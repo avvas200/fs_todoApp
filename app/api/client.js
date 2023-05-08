923e2d4850834df24b0a584eac7729794571f1e3
avvas200 <avvassani@gmail.com>
2023-05-08 10:47:58 +0100

    zfixes

--- app/api/client.js
+++ app/api/client.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://192.168.1.52:8000" });
+export default axios.create({ baseURL: "https://fs-todo.onrender.com" });
+// export default axios.create({ baseURL: "http://192.168.1.52:8000" });
